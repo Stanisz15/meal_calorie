61dae4027898f1e7909fb547e20848596f7caa25
Łukasz Staniszewski <lukasz.staniszewski@onet.pl>
2018-04-13 12:45:17 +0200

    starting models

--- diet/models.py
+++ diet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    describe = models.TextField(max_length=None)
+    calories = models.DecimalField(decimal_places=2, max_digits=8)
+    image = models.ImageField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    available = models.BooleanField(default=True)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=32)
+    products = models.ManyToManyField(Product)
